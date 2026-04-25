ee58b08377e22259091505eabee4da56c00bef84
Jeravima <jeravi61@gmail.com>
2026-04-25 12:10:01 -0500

    shadcn agregado

--- src/AppClima.tsx
+++ src/AppClima.tsx
@@ -1,17 +1,18 @@
-import { Search } from "lucide-react"
-
-
+import { useState } from "react";
+import { Search } from "./components/Search";
 
 export const AppClima = () => {
-  return (
-    <div className="flex w-full h-full">
-      <div className="bg-white shadow-xl min-w-4/5">
 
-     <h1>App Clima </h1>
-     <div>
-      <Search/>
-     </div>
+  const [inputValue, setInputValue] = useState('')
+  return (
+    <div className="flex w-full h-full m-4 ">
+      <div className="bg-white shadow-xl min-w-4/5 mx-auto  min-h-96 m-4">
+          <h1 className="text-2xl font-bold text-black justify-center flex mb-5">App Clima </h1>
+        <div className="justify-center flex">
+          <Search inputValue={inputValue} onChange={setInputValue} />
+        </div>
+        <h2>{inputValue}</h2>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,34 @@
-import { Button } from "./ui/button"
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 
-export const Search = () => {
-  return (
-    <div>
-        <form action="">
-            <input type="text" />
-            <Button/>
-        </form>
 
-    </div>
-  )
+interface Props{
+  inputValue:string
+  onChange: (value:string)=> void
 }
+
+export const Search = ({inputValue, onChange}:Props) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(value);
+    setValue("");
+  };
+  return (
+    <div >
+      <form action="" className="flex gap-4" onSubmit={handleSubmit}>
+        <Input
+          placeholder="Ingresa el nombre de tu ciudad"
+          type="text"
+          value={inputValue}
+          onChange={(e) => onChange(e.target.value) }
+          className="capitalize placeholder:normal-case p-1 w-lg"
+        />
+        <Button type="submit">Buscar</Button>
+      </form>
+    </div>
+  );
+};
