80f7c47e0a19b0b177d59726a0d35b6c944da503
Jeravima <jeravi61@gmail.com>
2026-05-19 13:10:19 -0500

    Cambios

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import { Switch } from "./ui/switch";
 
 interface Props {
   onSearch: (city: string) => void;
@@ -15,25 +16,31 @@ export const Navbar = ({ onSearch }: Props) => {
   };
 
   return (
-    <nav className="w-full px-4 md:px-6 py-4">
-      <form
-        onSubmit={submit}
-        className="flex items-center relative max-w-md md:max-w-xl lg:max-w-2xl"
-      >
-        <input
-          type="text"
-          placeholder="Buscar ciudad..."
-          value={city}
-          className="w-full rounded-2xl pl-10 pr-4 py-2 md:py-3 shadow-md focus:border-gray-400 focus:outline focus:outline-sky-500 capitalize text-sm md:text-base"
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="absolute left-3 text-gray-500 hover:text-gray-700 transition-colors"
+    <nav className="w-full px-4 md:px-6 py-4 shadow-md">
+      <div className="flex justify-between items-center">
+        <form
+          onSubmit={submit}
+          className="flex items-center relative max-w-md md:max-w-xl lg:max-w-2xl"
         >
-          <CiSearch size={25} />
-        </button>
-      </form>
+          <input
+            type="text"
+            placeholder="Buscar ciudad..."
+            value={city}
+            className="w-full rounded-2xl pl-10 pr-4 py-2 md:py-3  focus:border-gray-400 focus:outline focus:outline-sky-500 capitalize text-sm md:text-base"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="absolute left-3 text-gray-500 hover:text-gray-700 transition-colors"
+          >
+            <CiSearch size={25} />
+          </button>
+        </form>
+        <div className="mr-4">
+        <Switch  />
+
+        </div>
+      </div>
     </nav>
   );
 };
