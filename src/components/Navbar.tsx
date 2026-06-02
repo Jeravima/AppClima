578385490475292e9bd9123b3343b8c9946cd022
Jeravima <jeravi61@gmail.com>
2026-06-02 11:58:34 -0500

    Agregado Darkmode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { useTheme } from "@/context/theme-provider";
 import {Sun,Moon} from 'lucide-react'
+import { Input } from "./ui/input";
 
 interface Props {
   onSearch: (city: string) => void;
@@ -26,13 +27,14 @@ export const Navbar = ({ onSearch }: Props) => {
           onSubmit={submit}
           className="flex items-center relative max-w-md md:max-w-xl lg:max-w-2xl"
         >
-          <input
+          {/* <input
             type="text"
             placeholder="Buscar ciudad..."
             value={city}
             className="w-full rounded-2xl pl-10 pr-4 py-2 md:py-3  focus:border-gray-400 focus:outline focus:outline-sky-500 capitalize text-sm md:text-base"
             onChange={(e) => setCity(e.target.value)}
-          />
+          /> */}
+          <Input onChange={(e)=> setCity(e.target.value)} placeholder="Busca tu ciudad" value={city} type="text" className="pl-10 rounded-2xl capitalize placeholder:normal-case"/>
           <button
             type="submit"
             className="absolute left-3 text-gray-500 hover:text-gray-700 transition-colors"
@@ -43,6 +45,7 @@ export const Navbar = ({ onSearch }: Props) => {
         <div
           className="mr-4 cursor-pointer"
           onClick={() => setTheme(isDark ? "light" : "dark")}
+          title="Cambiar tema"
         >
           {isDark ? (
             <Sun className="h-6 w-6 text-yellow-400 transition-all" />
--- src/pages/homePage/MaxAndMin.tsx
+++ src/pages/homePage/MaxAndMin.tsx
@@ -9,13 +9,13 @@ export const MaxAndMin = ({ temp_min, temp_max }: Props) => {
   return (
     <div>
       <div className="flex  gap-5">
-        <div>
+        <div title="Temperatura maxima">
           <MoveUp color='red' className="drop-shadow-lg "/>
-          <p className="text-red-800">{Math.round(temp_max ?? 0)+2}°</p>
+          <p className="text-red-800 dark:text-red-300">{Math.round(temp_max ?? 0)+2}°</p>
         </div>
-        <div>
+        <div title="Temperatura minima">
           <MoveDown color="blue" className="drop-shadow-lg"/>
-          <p className="text-blue-800">{Math.round(temp_min ?? 0)}°</p>
+          <p className={`text-blue-800 dark:text-blue-300`}>{Math.round(temp_min ?? 0)}°</p>
         </div>
       </div>
     </div>
